4573553cadf3d8b6c8fc8fa07bd7660d4801de3b
Karina <rybalchenkokatina310@gmail.com>
2025-10-31 12:50:37 +0100

    Anderungen

--- src/h3/h3_main.java
+++ src/h3/h3_main.java
@@ -2,25 +2,25 @@ package h3;
 
 public class H3_main {
 	public static void main(String[] args) {
-		int i = 164;
-		int j = 117;
+		int i = 210;
+		int j = 102;
 		int k = -10;
-		
-		if(i > j) {
+
+		if (i > j) {
 			k = 1;
-			
-			if(i > 200) {
+
+			if (i > 200) {
 				k = 2;
-				
-				if(j > 100) {
+
+				if (j > 100) {
 					k = 3;
 				}
-			} 
-		} else if(i <= 200) {
-			if(j<= 100) {
+			}
+		} else if (i <= 200) {
+			if (j <= 100) {
 				k = 4;
 			}
 		}
-		System.out.println("k =" +k);
-	} 
+		System.out.println("k =" + k);
+	}
 }
